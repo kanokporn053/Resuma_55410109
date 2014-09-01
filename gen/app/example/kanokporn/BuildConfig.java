fbbaf0c69beae3f84ce68b3668c54e7e2c4e71b8
Kanokporn <pon-nana053@hotmail.com>
2014-09-01 17:11:21 +0700

    Resume First

---
+++ gen/app/example/kanokporn/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package app.example.kanokporn;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/app/example/kanokporn/ImageActivity.java
@@ -0,0 +1,14 @@
+package app.example.kanokporn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image_kanokporn);
+	}
+
+}
